fc2c7ed1029409b8a502e6ced33edd315924fe4f
SpiderX <85947756+GigaHertzLegacy-SpiderX@users.noreply.github.com>
2021-10-28 20:48:21 +0600

    Bugs fixed !
    Updated title and and turtle will not disappear it done his job !

--- France_Flag.py
+++ France_Flag.py
@@ -1,18 +1,19 @@
 from turtle import *
 
 wn = Screen()
+wn.title("Flag of France")
 
 penup()
-goto(-50,50)
+goto(-50, 50)
 pendown()
 
 color("blue")
 begin_fill()
 for count in range(2):
-  forward(50)
-  right(90)
-  forward(100)
-  right(90)
+    forward(50)
+    right(90)
+    forward(100)
+    right(90)
 end_fill()
 
 penup()
@@ -22,21 +23,22 @@ pendown()
 begin_fill()
 color("red")
 for count in range(2):
-  forward(50)
-  right(90)
-  forward(100)
-  right(90)
+    forward(50)
+    right(90)
+    forward(100)
+    right(90)
 end_fill()
 
 back(100)
 color("black")
 for i in range(2):
-  forward(150)
-  right(90)
-  forward(100)
-  right(90)
+    forward(150)
+    right(90)
+    forward(100)
+    right(90)
 
 penup()
-goto(25,70)
-write("Flag of France",align="center", font=("Arial", 15, "normal"))
+goto(25, 70)
+write("Flag of France", align="center", font=("Arial", 15, "normal"))
 hideturtle()
+done()
