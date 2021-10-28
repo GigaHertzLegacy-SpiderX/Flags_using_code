62c5a78c60a8ccdbcfdb49a30f5b07ee50dbaa9d
Anurag Nag Adoni <ananuragh@gmail.com>
2021-10-28 17:59:12 +0530

    France_Flag.py

---
+++ France_Flag.py
@@ -0,0 +1,42 @@
+from turtle import *
+
+wn = Screen()
+
+penup()
+goto(-50,50)
+pendown()
+
+color("blue")
+begin_fill()
+for count in range(2):
+  forward(50)
+  right(90)
+  forward(100)
+  right(90)
+end_fill()
+
+penup()
+forward(100)
+pendown()
+
+begin_fill()
+color("red")
+for count in range(2):
+  forward(50)
+  right(90)
+  forward(100)
+  right(90)
+end_fill()
+
+back(100)
+color("black")
+for i in range(2):
+  forward(150)
+  right(90)
+  forward(100)
+  right(90)
+
+penup()
+goto(25,70)
+write("Flag of France",align="center", font=("Arial", 15, "normal"))
+hideturtle()
