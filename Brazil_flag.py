39f03e489a416532bf54fcea40a4d02eafc2ffa9
jeffersonjpr <jefferson_michael407@hotmail.com>
2023-10-03 20:33:14 -0300

    improving brazilian flag

--- Brazil_flag.py
+++ Brazil_flag.py
@@ -1,47 +1,43 @@
 import turtle
+import math
 
 # Screen
 wn = turtle.Screen()
-wn.setup(width=800, height=600)
+wn.setup(width=900, height=600)
 wn.title("Brazil")
 wn.bgcolor("Green")
 
-
 # yellow losange
 turtle.penup()
-turtle.goto(-5, -180)
+turtle.goto(0, -270)
 turtle.pendown()
 turtle.color("Yellow")
 turtle.begin_fill()
-turtle.setheading(142)
-turtle.forward(300)
-turtle.setheading(40)
-turtle.forward(300)
-
-turtle.setheading(-40)
-turtle.forward(300)
-turtle.setheading(-141)
-turtle.forward(297)
+turtle.goto(-420, 0)
+turtle.goto(0, 270)
+turtle.goto(420, 0)
+turtle.goto(0, -270)
 turtle.end_fill()
 
+radius = 175
 # blue circle
 turtle.penup()
-turtle.goto(42, -47)
+turtle.goto(0, radius)
 turtle.pendown()
 turtle.begin_fill()
 turtle.color("Blue")
-turtle.circle(-85)
+turtle.circle(-radius)
 turtle.end_fill()
+turtle.penup()
 
 # white arc
-turtle.penup()
-turtle.goto(64, -10)
+turtle.goto(radius, -20)
 turtle.setheading(120)
 turtle.forward(1)
 turtle.pendown()
 turtle.setheading(120)
 turtle.color("White")
 turtle.pensize(10)
-turtle.circle(110, 92)
+turtle.circle(radius + 40, 110)
 
 turtle.done()
